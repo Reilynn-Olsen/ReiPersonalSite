f496e493156cc36b18caaf7752678c97298bb9c6
Reilynn <reiolsen00@gmail.com>
2022-09-25 12:16:53 -0600

    added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Header from './Header';
+import Welcome from './Welcome'
+import Projects from './Projects';
+import ContactMe from './ContactMe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Welcome />
+      <Projects/>
+      <ContactMe/>
     </div>
   );
 }
